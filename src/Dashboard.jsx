2090b1d32eab8ec965098c2eddcad73096a627fd
Darkvolcano <khangphamchuchi@gmail.com>
2024-06-19 20:41:47 +0700

    update dashboard api

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useContext } from "react";
-import './StyleDashboard.css';
-import { UserContext } from './UserContext';
+import React, { useState, useContext, useEffect } from "react";
+import "./StyleDashboard.css";
+import { UserContext } from "./UserContext";
+import axios from "axios";
 
 const Dashboard = () => {
-
+  const [user, setUser] = useState(0);
+  const [court, setCourt] = useState(0);
+  const [post, setPost] = useState(0);
+  const [venue, setVenue] = useState(0);
   const { loginMessage, setLoginMessage } = useContext(UserContext);
   const [noticeVisible, setNoticeVisible] = useState(false);
 
@@ -19,66 +23,134 @@ const Dashboard = () => {
       }
     };
     if (loginMessage) {
-        setTimeout(() => {
+      setTimeout(() => {
         setLoginMessage("Login successfully");
       }, 5000);
     }
   }, [loginMessage, setLoginMessage]);
 
-    const handleCloseNotice = () => {
-       setNoticeVisible(false);
-    };
+  const handleCloseNotice = () => {
+    setNoticeVisible(false);
+  };
 
-    return (
-        <section className="home-section">
-        <nav>
-          <div className="sidebar-button">
-            <i className="bx bx-menu sidebarBtn"></i>
-            <span className="dashboard">Dashboard</span>
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get("https://localhost:7088/api/Reports/Total-Account")
+      .then((result) => {
+        console.log(result.data);
+        setUser(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDataC();
+  }, []);
+
+  const getDataC = () => {
+    axios
+      .get("https://localhost:7088/api/Reports/Total-Courts")
+      .then((result) => {
+        console.log(result.data);
+        setCourt(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDataP();
+  }, []);
+
+  const getDataP = () => {
+    axios
+      .get("https://localhost:7088/api/Reports/Total-Post")
+      .then((result) => {
+        console.log(result.data);
+        setPost(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDataV();
+  }, []);
+
+  const getDataV = () => {
+    axios
+      .get("https://localhost:7088/api/Reports/Total-Revenue")
+      .then((result) => {
+        console.log(result.data);
+        setVenue(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <section className="home-section">
+      <nav>
+        <div className="sidebar-button">
+          <i className="bx bx-menu sidebarBtn"></i>
+          <span className="dashboard">Dashboard</span>
+        </div>
+      </nav>
+
+      <div className="home-content">
+        <div className="overview-boxes">
+          <div className="box">
+            <div className="right-side">
+              <div className="box-topic">Total Users</div>
+              <div className="number">{user}</div>
+            </div>
+            <i className="bx bx-cart-alt cart"></i>
           </div>
-        </nav>
-  
-        <div className="home-content">
-          <div className="overview-boxes">
-            <div className="box">
-              <div className="right-side">
-                <div className="box-topic">Total Users</div>
-                <div className="number">40,876</div>
-              </div>
-              <i className="bx bx-cart-alt cart"></i>
+          <div className="box">
+            <div className="right-side">
+              <div className="box-topic">Total Badminton Court</div>
+              <div className="number">{court}</div>
             </div>
-            <div className="box">
-              <div className="right-side">
-                <div className="box-topic">Total Badminton Court</div>
-                <div className="number">38,876</div>
-              </div>
-              <i className="bx bxs-cart-add cart two"></i>
-            </div>
-            <div className="box">
-              <div className="right-side">
-                <div className="box-topic">Total Register Court</div>
-                <div className="number">$12,876</div>
-              </div>
-              <i className="bx bx-cart cart three"></i>
-            </div>
-            <div className="box">
-              <div className="right-side">
-                <div className="box-topic">Total Return</div>
-                <div className="number">11,086</div>
-              </div>
-              <i className="bx bxs-cart-download cart four"></i>
-            </div>
-            {loginMessage && (
-                  <div className={`success ${noticeVisible ? '' : 'hide'}`}>
-                        <span className="check"><i className="fa fa-check-circle"></i></span>
-                        <span className="msg">{loginMessage}</span>
-                        <span className="crose" onClick={handleCloseNotice}><i className="fa fa-times"></i></span>
-                  </div>
-            )}
+            <i className="bx bxs-cart-add cart two"></i>
           </div>
+          <div className="box">
+            <div className="right-side">
+              <div className="box-topic">Total Post</div>
+              <div className="number">{post}</div>
+            </div>
+            <i className="bx bx-cart cart three"></i>
           </div>
-      </section>
-    );
+          <div className="box">
+            <div className="right-side">
+              <div className="box-topic">Total Venue</div>
+              <div className="number">{venue}</div>
+            </div>
+            <i className="bx bxs-cart-download cart four"></i>
+          </div>
+          {loginMessage && (
+            <div className={`success ${noticeVisible ? "" : "hide"}`}>
+              <span className="check">
+                <i className="fa fa-check-circle"></i>
+              </span>
+              <span className="msg">{loginMessage}</span>
+              <span className="crose" onClick={handleCloseNotice}>
+                <i className="fa fa-times"></i>
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
