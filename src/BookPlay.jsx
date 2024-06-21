ee83ae789ce33530afebfa0ea44e799736449195
Darkvolcano <khangphamchuchi@gmail.com>
2024-06-21 12:08:46 +0700

    update book infor

--- src/BookPlay.jsx
+++ src/BookPlay.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Book = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [courtName, setCourtName] = useState("");
+  const [openTime, setOpenTime] = useState("");
+  const [closeTime, setCloseTime] = useState("");
+  const [rules, setRules] = useState("");
+  const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [totalRate, setTotalRate] = useState("");
+  const [priceAvr, setPriceAvr] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get("https://localhost:7088/api/Courts?pageNumber=1&pageSize=10")
+      .then((result) => {
+        console.log(result.data);
+        setData(result.data.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault(); // Prevent form submission from reloading the page
+
+    const url = `https://localhost:7088/api/Courts/Search-Court?searchTerm=${search}&pageNumber=1&pageSize=10`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("Search result:", response.data.items);
+        setData(response.data.items);
+        toast.success("Search successfully");
+      })
+      .catch((error) => {
+        console.error("Error searching:", error);
+        toast.error("Failed to search courts");
+        setData([]);
+      });
+  };
+
   return (
     <div className="body-book">
+      <ToastContainer />
       <div className="book-fil">
         <div className="form-floating mb-3">
           <input
@@ -28,6 +80,36 @@ const Book = () => {
           </label>
         </div>
       </div>
+      <form
+        className="col-md-4"
+        style={{
+          width: "auto",
+          height: "auto",
+          display: "flex",
+          justifyContent: "center",
+          maxWidth: "100%",
+        }}
+        onSubmit={handleSearch}
+      >
+        <div className="form-floating">
+          <input
+            id="searchTerm"
+            type="text"
+            className="form-control"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            required
+          />
+          <label htmlFor="searchTerm" className="form-label">
+            Search court name
+          </label>
+        </div>
+        <div className="d-grid" style={{ margin: "0 12px" }}>
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </form>
       <div className="tooltip-container">
         <span className="tooltip">
           <span className="reject">Reject; Not Check In/Out Yet</span>
@@ -36,19 +118,27 @@ const Book = () => {
         </span>
         <span clasName="text">?</span>
       </div>
-      <div className="book-box">
-        <div className="book-infor">
-          <div className="book-text">
-            <div className="book-place">Sân cầu lông A</div>
-            <div className="book-slot">18:00 - 20:00</div>
-            <div className="book-price">Giá tham khảo:120K - 150K</div>
-            <div className="book-con">Tiêu chuẩn sân: Tiêu chuẩn quốc tế</div>
+      {data && data.length > 0 ? (
+        data.map((item, index) => (
+          <div className="book-box" key={index}>
+            <div className="book-infor">
+              <div className="book-text">
+                <div className="book-place">{item.courtName}</div>
+                <div className="book-slot">18:00 - 20:00</div>
+                <div className="book-price">Giá tham khảo:120K - 150K</div>
+                <div className="book-con">
+                  Tiêu chuẩn sân: Tiêu chuẩn quốc tế
+                </div>
+              </div>
+              <div className="book-image">
+                <img src={item.image} />
+              </div>
+            </div>
           </div>
-          <div className="book-image">
-            <img src="https://klgccwebsecurestoreprd01.blob.core.windows.net/klgccweb-prod/node/club-facility/images/2021-08/Badminton%20Court.jpg" />
-          </div>
-        </div>
-      </div>
+        ))
+      ) : (
+        <p>No court available</p>
+      )}
       <NavLink to="/BookInfor">Go to calendar</NavLink>
     </div>
   );
