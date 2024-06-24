e1bba49de8ace6443a5a51277b535270c55a3c83
Darkvolcano <khangphamchuchi@gmail.com>
2024-06-24 23:09:28 +0700

    update data grid booking type

--- src/BookingType.jsx
+++ src/BookingType.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
 import "./StyleDashboardAdmin.css";
-import Table from "react-bootstrap/Table";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -9,6 +8,11 @@ import Col from "react-bootstrap/Col";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 const BookingType = () => {
   useEffect(() => {
@@ -154,6 +158,41 @@ const BookingType = () => {
     }
   };
 
+  const columns = [
+    { field: "description", headerName: "Description" },
+    {
+      field: "status",
+      headerName: "Status",
+      renderCell: (params) => {
+        const { status } = params.row;
+        return status ? <TaskAltIcon /> : <HighlightOffIcon />;
+      },
+    },
+    {
+      field: "Action",
+      type: "Actions",
+      headerName: "Actions",
+      cellClassName: "actions",
+      renderCell: (params) => (
+        <>
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            color="inherit"
+            onClick={() => handleEdit(params.row.bookingTypeId)}
+          />
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            color="inherit"
+            onClick={() => handleDeleteShow(params.row.bookingTypeId)}
+          />
+        </>
+      ),
+    },
+  ];
+
   return (
     <>
       <Fragment>
@@ -179,160 +218,24 @@ const BookingType = () => {
             </div>
           </div>
 
-          <Table
-            striped
-            bordered
-            hover
-            style={{ margin: "14px 0", border: "none" }}
-          >
-            <thead>
-              <tr>
-                <th style={{ display: "none" }}>#</th>
-                <th
-                  className="center"
-                  style={{ borderTopLeftRadius: "10px", border: "none" }}
-                >
-                  Description
-                </th>
-                <th className="center">Status</th>
-                <th className="center" style={{ borderTopRightRadius: "10px" }}>
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {data && data.length > 0
-                ? data.map((item, index) => {
-                    return (
-                      <tr key={index}>
-                        <td style={{ display: "none" }}>{index + 1}</td>
-                        <td className="center">{item.description}</td>
-                        <td className="center">
-                          {item.status ? "Active" : "Disable"}
-                        </td>
-                        <td className="center">
-                          <svg
-                            width="33"
-                            height="33"
-                            viewBox="0 0 54 54"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            onClick={() => handleEdit(item.bookingTypeId)}
-                            style={{
-                              background: "#000",
-                              borderRadius: "30px",
-                              padding: "4px",
-                              cursor: "pointer",
-                            }}
-                          >
-                            <mask
-                              id="mask0_544_736"
-                              style={{ maskType: "alpha" }}
-                              maskUnits="userSpaceOnUse"
-                              x="0"
-                              y="0"
-                              width="54"
-                              height="54"
-                            >
-                              <path
-                                d="M28.0405 16.8232L13.6385 31.2252L13.6385 31.2252C12.993 31.8707 12.6702 32.1935 12.458 32.59C12.2458 32.9865 12.1563 33.4341 11.9772 34.3293L10.5415 41.5081C10.3394 42.5183 10.2384 43.0235 10.5258 43.3108C10.8131 43.5982 11.3182 43.4971 12.3285 43.2951L19.5073 41.8593L19.5073 41.8593C20.4025 41.6803 20.85 41.5908 21.2466 41.3786C21.6431 41.1664 21.9658 40.8436 22.6114 40.1981L37.0133 25.7961L28.0405 16.8232Z"
-                                fill="white"
-                              />
-                              <path
-                                d="M28.0405 16.8232L13.6385 31.2252L13.6385 31.2252C12.993 31.8707 12.6702 32.1935 12.458 32.59C12.2458 32.9865 12.1563 33.4341 11.9772 34.3293L10.5415 41.5081C10.3394 42.5183 10.2384 43.0235 10.5258 43.3108C10.8131 43.5982 11.3182 43.4971 12.3285 43.2951L19.5073 41.8593L19.5073 41.8593C20.4025 41.6803 20.85 41.5908 21.2466 41.3786C21.6431 41.1664 21.9658 40.8436 22.6114 40.1981L37.0133 25.7961L28.0405 16.8232Z"
-                                fill="black"
-                                fill-opacity="0.1"
-                              />
-                              <path
-                                d="M13.7635 31.5729L13.7635 31.5729C13.7328 31.6035 13.7024 31.6338 13.6724 31.6638C13.1697 32.1658 12.7583 32.5766 12.469 33.0874C12.1798 33.5983 12.0392 34.1624 11.8675 34.8517C11.8572 34.8929 11.8468 34.9345 11.8363 34.9766L10.6151 39.8616C10.609 39.8859 10.6029 39.9102 10.5968 39.9345C10.4757 40.4185 10.353 40.9086 10.3132 41.315C10.2699 41.7573 10.2861 42.4484 10.8372 42.9994C11.3882 43.5505 12.0793 43.5667 12.5216 43.5234C12.928 43.4837 13.4181 43.361 13.9021 43.2398C13.9264 43.2337 13.9507 43.2276 13.975 43.2216L18.86 42.0003C18.9021 41.9898 18.9437 41.9794 18.9849 41.9692C19.6742 41.7974 20.2384 41.6568 20.7492 41.3676C21.2601 41.0783 21.6708 40.6669 22.1728 40.1642C22.2028 40.1342 22.2331 40.1038 22.2638 40.0731L37.8981 24.4388L37.9315 24.4054C38.4708 23.8661 38.9255 23.4115 39.2697 23.0097C39.6297 22.5893 39.938 22.1501 40.1256 21.6203C40.4536 20.694 40.4536 19.6832 40.1256 18.7569C39.938 18.2272 39.6297 17.7879 39.2697 17.3676C38.9255 16.9658 38.4708 16.5111 37.9314 15.9718L37.8981 15.9385L37.8648 15.9051C37.3255 15.3658 36.8708 14.9111 36.469 14.5669C36.0487 14.2069 35.6094 13.8986 35.0797 13.7111C34.1534 13.3831 33.1426 13.3831 32.2163 13.7111C31.6865 13.8986 31.2473 14.2069 30.827 14.5669C30.4251 14.9111 29.9705 15.3658 29.4312 15.9052L29.3978 15.9385L13.8014 31.535L13.8013 31.535L13.7635 31.5729Z"
-                                stroke="white"
-                                stroke-width="2.50406"
-                              />
-                              <path
-                                d="M13.7635 31.5729L13.7635 31.5729C13.7328 31.6035 13.7024 31.6338 13.6724 31.6638C13.1697 32.1658 12.7583 32.5766 12.469 33.0874C12.1798 33.5983 12.0392 34.1624 11.8675 34.8517C11.8572 34.8929 11.8468 34.9345 11.8363 34.9766L10.6151 39.8616C10.609 39.8859 10.6029 39.9102 10.5968 39.9345C10.4757 40.4185 10.353 40.9086 10.3132 41.315C10.2699 41.7573 10.2861 42.4484 10.8372 42.9994C11.3882 43.5505 12.0793 43.5667 12.5216 43.5234C12.928 43.4837 13.4181 43.361 13.9021 43.2398C13.9264 43.2337 13.9507 43.2276 13.975 43.2216L18.86 42.0003C18.9021 41.9898 18.9437 41.9794 18.9849 41.9692C19.6742 41.7974 20.2384 41.6568 20.7492 41.3676C21.2601 41.0783 21.6708 40.6669 22.1728 40.1642C22.2028 40.1342 22.2331 40.1038 22.2638 40.0731L37.8981 24.4388L37.9315 24.4054C38.4708 23.8661 38.9255 23.4115 39.2697 23.0097C39.6297 22.5893 39.938 22.1501 40.1256 21.6203C40.4536 20.694 40.4536 19.6832 40.1256 18.7569C39.938 18.2272 39.6297 17.7879 39.2697 17.3676C38.9255 16.9658 38.4708 16.5111 37.9314 15.9718L37.8981 15.9385L37.8648 15.9051C37.3255 15.3658 36.8708 14.9111 36.469 14.5669C36.0487 14.2069 35.6094 13.8986 35.0797 13.7111C34.1534 13.3831 33.1426 13.3831 32.2163 13.7111C31.6865 13.8986 31.2473 14.2069 30.827 14.5669C30.4251 14.9111 29.9705 15.3658 29.4312 15.9052L29.3978 15.9385L13.8014 31.535L13.8013 31.535L13.7635 31.5729Z"
-                                stroke="black"
-                                stroke-opacity="0.1"
-                                stroke-width="2.50406"
-                              />
-                              <path
-                                d="M28.04 16.8232L37.0129 25.7961"
-                                stroke="white"
-                                stroke-width="2.50406"
-                              />
-                              <path
-                                d="M28.04 16.8232L37.0129 25.7961"
-                                stroke="black"
-                                stroke-opacity="0.1"
-                                stroke-width="2.50406"
-                              />
-                            </mask>
-                            <g mask="url(#mask0_544_736)">
-                              <rect
-                                x="-10.0234"
-                                y="-0.12793"
-                                width="64.1115"
-                                height="59.1798"
-                                fill="#F49D1A"
-                              />
-                            </g>
-                          </svg>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="icon icon-tabler icon-tabler-trash"
-                            width="33"
-                            height="33"
-                            viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            onClick={() => handleDeleteShow(item.bookingTypeId)}
-                            style={{
-                              background: "#000",
-                              borderRadius: "30px",
-                              padding: "4px",
-                              cursor: "pointer",
-                            }}
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <line
-                              x1="4"
-                              y1="7"
-                              x2="20"
-                              y2="7"
-                              style={{ color: "red" }}
-                            />
-                            <line
-                              x1="10"
-                              y1="11"
-                              x2="10"
-                              y2="17"
-                              style={{ color: "red" }}
-                            />
-                            <line
-                              x1="14"
-                              y1="11"
-                              x2="14"
-                              y2="17"
-                              style={{ color: "red" }}
-                            />
-                            <path
-                              d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"
-                              style={{ color: "red" }}
-                            />
-                            <path
-                              d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"
-                              style={{ color: "red" }}
-                            />
-                          </svg>
-                        </td>
-                      </tr>
-                    );
-                  })
-                : "Loading..."}
-            </tbody>
-          </Table>
+          <div style={{ width: "100%" }}>
+            <DataGrid
+              getRowId={(data) => data.bookingTypeId}
+              columns={columns}
+              rows={data}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 7,
+                  },
+                },
+              }}
+              pageSizeOptions={[7]}
+              sx={{
+                width: "-webkit-fill-available",
+              }}
+            />
+          </div>
 
           <Modal
             show={showCreateModal}
