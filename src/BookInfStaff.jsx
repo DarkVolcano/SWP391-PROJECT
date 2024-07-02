84ba82386ecbdcff419ea81921441b9487394b34
Darkvolcano <khangphamchuchi@gmail.com>
2024-07-02 10:14:07 +0700

    update book information for staff

--- src/BookInfStaff.jsx
+++ src/BookInfStaff.jsx
@@ -6,8 +6,11 @@ import "react-toastify/dist/ReactToastify.css";
 const BookInStaff = () => {
   const [bookingDetailId, setBookingDetailId] = useState("");
   const [subCourtId, setSubCourtId] = useState("");
+  const [bookingDetails, setBookingDetails] = useState(null);
+  const [showInputForm, setShowInputForm] = useState(true);
 
-  const handleSave = () => {
+  const handleSave = (e) => {
+    e.preventDefault();
     const url = "https://localhost:7088/api/Bookings/CheckIn";
     const data = {
       bookingDetailId: bookingDetailId,
@@ -19,9 +22,11 @@ const BookInStaff = () => {
       .then((result) => {
         clear();
         toast.success("Check in successfully");
+        setBookingDetails(result.data);
+        setShowInputForm(false); // Hide the input form upon successful check-in
       })
       .catch((error) => {
-        toast.error(error);
+        toast.error("Error checking in");
         console.log(error);
       });
   };
@@ -39,37 +44,96 @@ const BookInStaff = () => {
     <div className="body-staff">
       <ToastContainer />
       <div className="check-in">THÔNG TIN KHÁCH HÀNG</div>
-      <form className="check-input" onSubmit={handleSave}>
-        <div className="form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="outputCourtid"
-            placeholder="Court id"
-            value={bookingDetailId}
-            onChange={(e) => setBookingDetailId(e.target.value)}
-          ></input>
-          <label htmlFor="outputCourtid" className="form-label">
-            Booking id
-          </label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="outputBookingid"
-            placeholder="Booking id"
-            value={subCourtId}
-            onChange={(e) => setSubCourtId(e.target.value)}
-          ></input>
-          <label htmlFor="outputBookingid" className="form-label">
-            Subcourt id
-          </label>
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Tiếp tục
-        </button>
-      </form>
+      {showInputForm && ( // Show the input form only if showInputForm state is true
+        <form className="check-input" onSubmit={handleSave}>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="outputCourtid"
+              placeholder="Court id"
+              value={bookingDetailId}
+              onChange={(e) => setBookingDetailId(e.target.value)}
+            ></input>
+            <label htmlFor="outputCourtid" className="form-label">
+              Booking id
+            </label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="outputBookingid"
+              placeholder="Booking id"
+              value={subCourtId}
+              onChange={(e) => setSubCourtId(e.target.value)}
+            ></input>
+            <label htmlFor="outputBookingid" className="form-label">
+              Subcourt id
+            </label>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Tiếp tục
+          </button>
+        </form>
+      )}
+      {bookingDetails &&
+        !showInputForm && ( // Show booking details form only if bookingDetails is not null and showInputForm is false
+          <form className="check-input">
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="outputCourtid"
+                placeholder="Court id"
+                value={bookingDetails.courtName.trim()}
+                disabled // Disable input fields for booking details
+              ></input>
+              <label htmlFor="outputCourtid" className="form-label">
+                Sân
+              </label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="outputBookingid"
+                placeholder="Booking id"
+                value={bookingDetails.subCourtName.trim()}
+                disabled
+              ></input>
+              <label htmlFor="outputBookingid" className="form-label">
+                Sân nhỏ
+              </label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="outputBookingid"
+                placeholder="Booking id"
+                value={bookingDetails.slotTimeStart.trim()}
+                disabled
+              ></input>
+              <label htmlFor="outputBookingid" className="form-label">
+                Thời gian bắt đầu
+              </label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="outputBookingid"
+                placeholder="Booking id"
+                value={bookingDetails.slotTimeEnd.trim()}
+                disabled
+              ></input>
+              <label htmlFor="outputBookingid" className="form-label">
+                Thời gian kết thúc
+              </label>
+            </div>
+          </form>
+        )}
     </div>
   );
 };
